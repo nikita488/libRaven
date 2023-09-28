c7d4160c12bbd6a391add05d57889277d6afa375
nikita488 <6237881+nikita488@users.noreply.github.com>
2023-09-28 14:32:47 +0300

    Debug config fix

--- premake5.lua
+++ premake5.lua
@@ -11,7 +11,7 @@ workspace "libRaven"
 	
 	filter "configurations:Debug"
 		libdirs { path.join(IG_ROOT, "DirectX9/libdbg") }
-		defines { "WIN32", "_DEBUG", "_WINDOWS", "_USRDLL", "LIBRAVEN_EXPORT", "LIBRAVEN_DYNAMIC", "IG_GFX_DX9", "IG_TARGET_WIN32", "IG_TARGET_TYPE_WIN32", "IG_ALCHEMY_DLL", "IG_NO_MEMORY_TRACKING_MALLOC_MACROS" }
+		defines { "WIN32", "_DEBUG", "_WINDOWS", "_USRDLL", "LIBRAVEN_EXPORT", "LIBRAVEN_DYNAMIC", "IG_GFX_DX9", "IG_TARGET_WIN32", "IG_TARGET_TYPE_WIN32", "IG_ALCHEMY_DLL", "IG_DEBUG" }
 		symbols "On"
 		postbuildcommands "copy /y \"$(TargetDir)\" \"..\\libdbg\\\""
 	filter {}
@@ -25,7 +25,7 @@ workspace "libRaven"
 	
 	includedirs { path.join(IG_ROOT, "include") }
 	
-	links { "libIGCore", "libIGAttrs" }
+	links { "libIGCore", "libIGAttrs", "libIGGfx" }
    
 	files { "src/*.*" }
    
